3cfb581b77e1c23c7be04ee37d6dfb9ad5c2a189
albertoomartinn <alberto.martin@teinor.es>
2022-02-17 22:30:49 +0100

    store configured

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { rootReducer } from '../reducers/rootReducer'
+import { rootReducer } from '../reducers/rootReducers'
 
-const composeEnhancers = (typeof window !== 'undefined' && window window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 export const store = createStore(
     rootReducer,
