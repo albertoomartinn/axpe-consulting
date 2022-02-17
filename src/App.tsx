4a23682a1be1065f1f7e8b13df6662933051fb77
albertoomartinn <alberto.martin@teinor.es>
2022-02-17 22:31:56 +0100

    configured App with provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Provider } from 'react-redux';
 import MapScreen from './screen/MapScreen';
+import { store } from './store/store';
 
 function App() {
-  return <MapScreen />
+  return (
+    <Provider store={store}>
+      <MapScreen />
+    </Provider>
+  )
 }
 
 export default App;
