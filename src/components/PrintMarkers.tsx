33596f449c55f1485c7f1fe15a6fc2da9b6a4511
albertoomartinn <alberto.martin@teinor.es>
2022-02-18 03:01:11 +0100

    delete comment

--- src/components/PrintMarkers.tsx
+++ src/components/PrintMarkers.tsx
@@ -12,12 +12,6 @@ const PrintMarkers: React.FC<PrintMarkersProps> = ({ markersList }) => {
         const { lat, lng } = marker
         items.push(<Marker key={key} position={{ lat, lng }} options={{ label: { text: marker.locationName, className: 'map-marker' } }} />)
     });
-    // markersList.map(async (marker: any, key: any) => {
-    //     console.log(marker)
-    //     const coords: google.maps.LatLng | google.maps.LatLngLiteral = await getLatAndLng(marker)
-    //     console.log(coords)
-    //     items.push(<Marker key={key} position={{ lat: 37.2430548, lng: -115.7930198 }} options={{ label: { text: 'marker.formatted_address', className: 'map-marker' } }} />)
-    // })
     
     return (
         <>
